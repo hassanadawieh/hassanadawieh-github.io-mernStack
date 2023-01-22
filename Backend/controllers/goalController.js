4514b24fd5b59d97b0fa5ef1c7d20f0772639112
Hassan Adawie <adawiehhassan@gmail.com>
2023-01-22 13:37:31 +0200

    Initial_Rest_API_for_Goals

--- Backend/controllers/goalController.js
+++ Backend/controllers/goalController.js
@@ -1,11 +1,16 @@
 const asyncHandler = require('express-async-handler')
 
+
+
+const Goal = require('../models/goalModel')
 //@desc      Get goals
 //@route     Get /api/goals
 //@access    private
 
 const getGoal = asyncHandler( async (req , res ) => {
-    res.status(200).json({ message : `Get Goals`})
+    const goals = await Goal.find()
+
+    res.status(200).json(goals)
 })
 
 //@desc      Set goals
@@ -18,7 +23,11 @@ if(!req.body.text){
     throw new Error('please add a text field')
 }
 
-     res.status(200).json({ message: "Set Goals" });
+const goal = await Goal.create({
+    text : req.body.text
+})
+
+     res.status(200).json(goal);
 })
 
 //@desc      Update goals
@@ -26,7 +35,19 @@ if(!req.body.text){
 //@access    private
 
 const updateGoal =asyncHandler( async (req , res ) => {
-    res.status(200).json({ message: `Update Goal ${req.params.id}` });
+    const goal = await Goal.findById(req.params.id)
+
+if(!goal) {
+    res.status(404)
+    throw new Error ('Goal not found')
+}
+const updatedGoal = await Goal.findByIdAndUpdate(req.params.id,
+     req.body,
+      {
+        new :true,
+    })
+
+    res.status(200).json(updatedGoal);
 })
 
 //@desc      Delete goals
@@ -34,7 +55,17 @@ const updateGoal =asyncHandler( async (req , res ) => {
 //@access    private
 
 const deleteGoal = asyncHandler( async (req , res ) => {
-    res.status(200).json({ message: `Delete Goal ${req.params.id}` });
+        const goal = await Goal.findById(req.params.id);
+
+        if (!goal) {
+          res.status(404);
+          throw new Error("Goal not found");
+        }
+
+        await goal.remove()
+        
+
+        res.status(200).json({id : req.params.id});
 })
 
 
--- Backend/server.js
+++ Backend/server.js
@@ -1,15 +1,30 @@
 const express = require('express');
+const colors = require('colors');
 const dotenv = require('dotenv').config();
-const PORT = process.env.PORT || 5000;
-const {errorHandler} = require('./middleware/errorMiddleware')
-const app = express()
-
-app.use(express.json())
-app.use(express.urlencoded({ extended : false }))
-
-app.use('/api/goals', require('./routes/goalRoutes'))
+const {errorHandler} = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db');
+const PORT = process.env.PORT || 6000;
 
 
-app.use(errorHandler)
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+//sentry
+
+// const Sentry = require("@sentry/node");
+// Sentry.init({ dsn: "https://<key>@sentry.io/<project>" });
+
+//--------------------------------------------------------------
+
+connectDB()
+
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended : false }));
+
+app.use('/api/goals', require('./routes/goalRoutes'));
+
+
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
